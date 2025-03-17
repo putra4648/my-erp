4b39bb7f9d5127814fd57ffdbd822f1fe1dcb08a
Walkers <wakwaujaim@gmail.com>
2025-03-17 11:18:09 +0700

    :sparkles: feat: 1 module is missing

---
+++ library/src/test/java/id/putra/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package id.putra.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/id/putra/myerp/MyErpApplication.java
@@ -0,0 +1,13 @@
+package id.putra.myerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyErpApplication.class, args);
+	}
+
+}
