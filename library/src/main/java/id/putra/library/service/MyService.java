b5654905d4b641400601ff9f73e3abe6b9eac46e
Walkers <wakwaujaim@gmail.com>
2025-03-17 11:10:01 +0700

    :tada: #MulaiAjaDulu

---
+++ library/src/main/java/id/putra/library/service/MyService.java
@@ -0,0 +1,10 @@
+package id.putra.library.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    public String messsage() {
+        return "Hello World";
+    }
+}
